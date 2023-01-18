9f6df180595c51ef97778c00150ff1646a2fe0fd
emiezeoc <emiezeoc@gmail.com>
2023-01-18 10:49:08 -0300

    fix: Arregla indentacion y cambia nombre de funcion

--- tarea-1/tarea-1.js
+++ tarea-1/tarea-1.js
@@ -1,27 +1,20 @@
-
-
-
-
 document.querySelector("#calcular-salario-mensual").onclick = function() {
-
-const $salarioAnual= Number (document.querySelector("#ingresar-salario-anual").value);
-const salarioMensual= dividirPorDoce($salarioAnual)
-
-dividirPorDoce($salarioAnual)
-mostrarResultado(salarioMensual)
-
-
-return false;
+    const $salarioAnual= Number (document.querySelector("#ingresar-salario-anual").value);
+    const salarioMensual= dividirPorMeses($salarioAnual)
+    
+    dividirPorMeses($salarioAnual)
+    mostrarResultado(salarioMensual)
+    
+    return false;
 }
 
-function dividirPorDoce($salarioAnual){
-
-return $salarioAnual/12;
+function dividirPorMeses($salarioAnual) {
+    const mesesDelAnio= 12
+    return $salarioAnual/mesesDelAnio;
 }
 
-function mostrarResultado(salarioMensual){
-
-document.querySelector("#salario-mensual").value = salarioMensual
-
-return false; 
+function mostrarResultado(salarioMensual) {
+    document.querySelector("#salario-mensual").value = salarioMensual
+    
+    return false; 
 }
