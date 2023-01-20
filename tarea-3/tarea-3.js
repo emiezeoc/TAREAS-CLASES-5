7cff1c12d45ed97ff9e71424733cd95f942c2956
emiezeoc <emiezeoc@gmail.com>
2023-01-20 11:00:24 -0300

    fix: Ordena el codigo

--- tarea-3/tarea-3.js
+++ tarea-3/tarea-3.js
@@ -1,71 +1,55 @@
-
-
 const $botonCrearFormulario = document.querySelector("#crear-formulario");
 let $cantidadDeClases;
 
 $botonCrearFormulario.onclick = function(){
     $cantidadDeClases = Number(document.querySelector("#cantidad-clases").value);
 
-crearFormularios($cantidadDeClases)
+    crearFormularios($cantidadDeClases)
 
-
-return $cantidadDeClases;
+    return $cantidadDeClases;
 }
 
-
-
-
 function crearFormularios($cantidadDeClases){
-
-const nodoPagina = document.querySelector("div")
+    const nodoPagina = document.querySelector("div")
 
     for (let i = 0; i < $cantidadDeClases; i++) {
 
-
-let $labelHoras = document.createElement("label")
-$labelHoras.for= `horas-${i+1}`
-$labelHoras.textContent= `horas de clase-${i+1}`
-    
-    
-    
-let $inputsHoras = document.createElement("input")
+    let $labelHoras = document.createElement("label")
+    $labelHoras.for= `horas-${i+1}`
+    $labelHoras.textContent= `horas de clase-${i+1}`
+        
+    let $inputsHoras = document.createElement("input")
     $inputsHoras.id = `horas-${i+1}`
     $inputsHoras.className= "horas"
     $inputsHoras.placeholder= "hs"
 
- 
-
-
-let $labelMinutos = document.createElement("label")
+    let $labelMinutos = document.createElement("label")
     $labelMinutos.for= `minutos-${i+1}`
     $labelMinutos.textContent= `minutos de clase-${i+1}`
 
-
-
-let $inputsMinutos = document.createElement("input")
+    let $inputsMinutos = document.createElement("input")
     $inputsMinutos.id = `minutos-${i+1}`
     $inputsMinutos.className= "minutos"
     $inputsMinutos.placeholder= "mins"
 
-
-let $labelSegundos = document.createElement("label")
+    let $labelSegundos = document.createElement("label")
     $labelSegundos.for = `segundos-${i+1}`
     $labelSegundos.textContent = `segundos de clase- ${i+1}`
     
-let $inputsSegundos = document.createElement("input")
+    let $inputsSegundos = document.createElement("input")
     $inputsSegundos.id = `segundos-${i+1}`
     $inputsSegundos.className = "segundos"
     $inputsSegundos.placeholder = "seg"
 
-nodoPagina.appendChild($labelHoras)
-nodoPagina.appendChild($inputsHoras)
+    nodoPagina.appendChild($labelHoras)
+    nodoPagina.appendChild($inputsHoras)
 
-nodoPagina.appendChild($labelMinutos)
-nodoPagina.appendChild($inputsMinutos)
+    nodoPagina.appendChild($labelMinutos)
+    nodoPagina.appendChild($inputsMinutos)
 
-nodoPagina.appendChild($labelSegundos)
-nodoPagina.appendChild($inputsSegundos)
-    }
+    nodoPagina.appendChild($labelSegundos)
+    nodoPagina.appendChild($inputsSegundos)
+}
 
 return false;
 }
