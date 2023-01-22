05f788088d97ca0d3c9892261d672556e1f62a1e
emiezeoc <emiezeoc@gmail.com>
2023-01-22 01:20:02 -0300

    Update tarea-1.js

--- tarea-1/tarea-1.js
+++ tarea-1/tarea-1.js
@@ -1,27 +1,21 @@
-
-
-
-
 document.querySelector("#calcular-salario-mensual").onclick = function() {
+    const $salarioAnual= Number (document.querySelector("#ingresar-salario-anual").value);
+    const salarioMensual= dividirPorDoce($salarioAnual)
 
-const $salarioAnual= Number (document.querySelector("#ingresar-salario-anual").value);
-const salarioMensual= dividirPorDoce($salarioAnual)
+    dividirPorDoce($salarioAnual)
+    mostrarResultado(salarioMensual)
 
-dividirPorDoce($salarioAnual)
-mostrarResultado(salarioMensual)
-
-
-return false;
+    return false;
 }
 
 function dividirPorDoce($salarioAnual){
 
-return $salarioAnual/12;
+    return $salarioAnual/12;
 }
 
 function mostrarResultado(salarioMensual){
 
-document.querySelector("#salario-mensual").value = salarioMensual
+    document.querySelector("#salario-mensual").value = salarioMensual
 
-return false; 
+    return false; 
 }
