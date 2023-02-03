1f2f1d1724c4ce826f7303fa6ef9a8a42637c558
emiezeoc <emiezeoc@gmail.com>
2023-02-02 22:52:01 -0300

    Update tarea-4.js

--- tarea-4/tarea-4.js
+++ tarea-4/tarea-4.js
@@ -1,8 +1,28 @@
 const nodoLista = document.querySelectorAll("li")
-
 let arrayDeNumeros = []
+const $botonCalcularPromedio = document.querySelector("#boton-calcular-promedio");
+const $botonObtenerElNumeroMenor = document.querySelector("#boton-obtener-numero-menor");
+const $botonObtenerElNumeroMayor = document.querySelector("#boton-obtener-numero-mayor");
+const $botonObtenerModa = document.querySelector("#boton-obtener-moda")
 
-for (let i = 0; i < nodoLista.length; i++) {
+
+
+$botonCalcularPromedio.onclick = function() {
+    for (let i = 0; i < nodoLista.length; i++) {
+        arrayDeNumeros.push (Number(nodoLista[i].textContent))
+    }
+    calcularPromedio(arrayDeNumeros)
     
-    arrayDeNumeros.push (Number(nodoLista[i].textContent))
+    return false; 
+}
+
+function calcularPromedio(arrayDeNumeros){
+
+    let sumatoriaDelArray = 0
+    for (let i = 0; i < arrayDeNumeros.length; i++) { 
+        sumatoriaDelArray += arrayDeNumeros[i];
+        promedioDelArray = sumatoriaDelArray/arrayDeNumeros.length
+    }
+    
+    return console.log(promedioDelArray);
 }
